7ad5d11f0d585681d4aa038d51415d5539931176
Sweeney Corniea <sweenfi@icloud.com>
2023-11-02 12:27:52 -0600

    testing upload settings

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const CLIENTDEVPORT = 5173
 app.use(cors({ origin: process.env.CLIENT_ORIGIN || `http://localhost:${CLIENTDEVPORT}` }))
    
 app.use(logger('dev'));
+app.use(cors())
 app.use(express.json());
 
 
