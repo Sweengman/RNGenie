35c3971e9ad93f0c262cb51d0250c2eedd2bb9e4
Sweeney Corniea <sweenfi@icloud.com>
2023-11-09 20:51:02 -0700

    added nodemon to devDependancies

--- src/utilities/maps-api.js
+++ src/utilities/maps-api.js
@@ -1,5 +1,5 @@
 import sendRequest from './send-request'
-const BASE_URL = 'https://rngeniecli.onrender.com/api/maps'
+const BASE_URL = 'https://rngenie.onrender.com/api/maps'
 
 export function saveMap(mapData) {
     return sendRequest(BASE_URL + '/new', 'POST', mapData)
--- src/utilities/users-api.js
+++ src/utilities/users-api.js
@@ -1,6 +1,5 @@
 import sendRequest from './send-request'
-const BASE_URL = 'https://rngeniecli.onrender.com/api/users'
-
+const BASE_URL = 'https://rngenie.onrender.com/api/users'
 export function signUp(userData) {
     return sendRequest(BASE_URL, 'POST', userData);
   }
